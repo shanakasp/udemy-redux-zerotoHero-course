5c5d89d139086e86b3f3754b7d47cf0445d7da45
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-06-09 17:56:30 +0530

    added 3 files to redux

---
+++ src/redux/reducers.js
@@ -0,0 +1,18 @@
+import { DECREMENT, INCREMENT } from "./constraints";
+
+const initialState = {
+  value: 0,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return { ...state, value: state.value + 1 };
+    case DECREMENT:
+      return { ...state, value: state.value - 1 };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
